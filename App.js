42484c3ba8e78b40c053f815ea6d9a51296367ed
Chanteng <yrnchateng@gmail.com>
2022-01-26 04:52:20 -0800

    first-app

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.img} source={require("./assets/2.jpg")} />
+
+        <View style={styles.mother}>
+      <Text style={styles.text}>Name</Text>
+      <Text style={styles.text}>MR. CEO</Text>
+      </View>
+
+      <View style={styles.mother}>
+      <Text style={styles.text}>Email</Text>
+      <Text style={styles.text}>something@mr.ceo</Text>
+      </View>
+
+      <View style={styles.mother}>
+      <Text style={styles.text}>Gender</Text>
+      <Text style={styles.text}>Kwame</Text>
+      </View>
+
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+
+
+const styles = StyleSheet.create( {
+container: {
+    backgroundColor: "black",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+},
+
+text: {
+    fontSize: 30,
+    color: "white",
+    borderWidth: 3,
+    borderColor: "white",
+    paddingVertical: 5,
+    flex: 2,
+    paddingHorizontal: 20
+},
+
+img: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    alignSelf: "center",
+    marginTop: 50,
+    marginBottom: 50
+},
+
+mother: {
+    flexDirection: "row",
+    alignSelf: "center"
+}
+
+})
+
+export default App;
